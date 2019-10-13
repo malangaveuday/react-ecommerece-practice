eac40c60a825a58e8e6ef8fc98b691df32a0e415
UdayMalangave <malangaveuday23@gmail.com>
2019-10-13 17:37:21 +0530

    bootup project from create react app

---
+++ src/home/HomePageContainer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function HomePageContainer() {
+  return (
+    <div>
+      <div className="directory-container">
+        <div className="menu-container">
+          <div className="menu-content">
+            <h1 className="title">HATS</h1>
+            <span className="title">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+      <div className="directory-container">
+        <div className="menu-container">
+          <div className="menu-content">
+            <h1 className="title">JACKETS</h1>
+            <span className="title">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+      <div className="directory-container">
+        <div className="menu-container">
+          <div className="menu-content">
+            <h1 className="title">SNICKERS</h1>
+            <span className="title">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+      <div className="directory-container">
+        <div className="menu-container">
+          <div className="menu-content">
+            <h1 className="title">WOMEN</h1>
+            <span className="title">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+      <div className="directory-container">
+        <div className="menu-container">
+          <div className="menu-content">
+            <h1 className="title">MEN</h1>
+            <span className="title">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
