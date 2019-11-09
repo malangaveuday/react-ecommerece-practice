71c7ced1906b1ce2eab4334f82cd4da1e186e982
malanagveuday <malangaveuday23@gmail.com>
2019-11-09 12:46:56 +0530

    changes for router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePageContainer from './components/home/HomePageContainer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+const Hats = () => <h1>Hats</h1>
 
 function App() {
   return (
     <div className="App">
-      <HomePageContainer />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePageContainer} />
+          <Route path="/shop/:shopItem" component={Hats} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/menu/MenuItem.jsx
+++ src/components/menu/MenuItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './menu-item.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ item }) => (
-  <div className={`${item.size} menu-item`}>
+//  match, history comes from withRouter HOF.
+
+const MenuItem = ({ item, match, history }) => (
+  <div className={`${item.size} menu-item`} onClick={() => history.push(`${match.url}${item.linkUrl}`)}>
     <div className="background-image" style={{ backgroundImage: `url(${item.imageUrl})` }} /> 
     <div className='content'>
       <div className='title'>{item.title}</div>
@@ -11,4 +14,4 @@ const MenuItem = ({ item }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
